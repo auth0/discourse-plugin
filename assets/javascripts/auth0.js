a8928a23ff11182ca34ddc083137a69684970270
Alexis Luque <aluque@indexlabs.com.uy>
2018-03-27 14:54:47 -0300

    Update to use universal login instead of Lock

--- assets/javascripts/auth0.js
+++ assets/javascripts/auth0.js
@@ -1,46 +1,4 @@
-/* global Auth0Lock */
 (function () {
-  function appendScript(src, callback) {
-    var new_script = document.createElement('script');
-    new_script.setAttribute('src',src);
-    new_script.onload = callback;
-    document.head.appendChild(new_script);
-  }
-
-  var lock;
-
-  var script_url = '//cdn.auth0.com/js/lock-9.2.js';
-
-  appendScript(script_url, function () {
-    var checkInterval = setInterval(function () {
-      if (!Discourse.SiteSettings) {
-        return;
-      }
-
-      clearInterval(checkInterval);
-
-      if (!Discourse.SiteSettings.auth0_client_id) {
-        return;
-      }
-
-      var client_id = Discourse.SiteSettings.auth0_client_id;
-      var domain = Discourse.SiteSettings.auth0_domain;
-
-      lock = new Auth0Lock(client_id, domain);
-
-    }, 300);
-  });
-
-  var LoginController = require('discourse/controllers/login').default;
-  LoginController.reopen({
-    authenticationComplete: function () {
-      if (lock) {
-        lock.hide();
-      }
-      return this._super.apply(this, arguments);
-    }
-  });
-
   var ApplicationRoute = require('discourse/routes/application').default;
   ApplicationRoute.reopen({
     actions: {
@@ -48,14 +6,7 @@
         if (!Discourse.SiteSettings.auth0_client_id || Discourse.SiteSettings.auth0_connection !== '') {
           return this._super();
         }
-
-        lock.show({
-          popup:        true,
-          responseType: 'code',
-          callbackURL:  Discourse.SiteSettings.auth0_callback_url
-        });
-
-        this.controllerFor('login').resetForm();
+        window.location.href = "/auth/auth0";
       },
       showCreateAccount: function () {
         if (!Discourse.SiteSettings.auth0_client_id || Discourse.SiteSettings.auth0_connection !== '') {
@@ -65,19 +16,9 @@
         var createAccountController = Discourse.__container__.lookup('controller:createAccount');
 
         if (createAccountController && createAccountController.accountEmail) {
-          if (lock) {
-            lock.hide();
-            Discourse.Route.showModal(this, 'createAccount');
-          } else {
-            this._super();
-          }
+          this._super();
         } else {
-          lock.show({
-            mode:         'signup',
-            popup:        true,
-            responseType: 'code',
-            callbackURL:  Discourse.SiteSettings.auth0_callback_url
-          });
+          window.location.href = "/auth/auth0";
         }
       }
     }
